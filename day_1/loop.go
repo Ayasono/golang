a47f0e9170376b0ab63914a386824ef047517924
Ayasono <yuehu@ualberta.ca>
2023-02-04 22:44:33 +0800

    "commit all files"

--- day_1/loop.go
+++ day_1/loop.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	var n = rand.Intn(10)
+
+	if n > 4 {
+		fmt.Printf("success")
+	} else {
+		println("failed")
+	}
 }
